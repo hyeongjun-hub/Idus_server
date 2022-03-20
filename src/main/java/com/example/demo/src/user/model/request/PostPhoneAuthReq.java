dbdcae11539a4c9cb5f67455e30f1684da1a2bd6
hyeongjun <chogudwns@naver.com>
2022-03-21 01:36:29 +0900

    feat: Implement login & kakaoLogin feature 로그인과 카카오 로그인 기능을 구현하고 api 생성

--- src/main/java/com/example/demo/src/user/model/request/PostPhoneAuthReq.java
+++ src/main/java/com/example/demo/src/user/model/request/PostPhoneAuthReq.java
@@ -5,11 +5,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class PostPhoneAuthReq {
+    @NotBlank
+    // todo: regex valid
     private String phone;
     private String authNumber;
 }
