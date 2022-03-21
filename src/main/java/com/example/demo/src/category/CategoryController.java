4fe28923fcabc2dc34903cb573df634441873707
hyeongjun <chogudwns@naver.com>
2022-03-22 07:06:11 +0900

    feat: Implement getCategories function 카테고리들을 불러오는 api를 구현

--- src/main/java/com/example/demo/src/category/CategoryController.java
+++ src/main/java/com/example/demo/src/category/CategoryController.java
@@ -3,6 +3,7 @@ package com.example.demo.src.category;
 import com.example.demo.config.BaseException;
 import com.example.demo.config.BaseResponse;
 import com.example.demo.src.category.model.response.GetCategoryRes;
+import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,20 +13,16 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
+@AllArgsConstructor
 public class CategoryController {
     final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     private final CategoryProvider categoryProvider;
 
-    @Autowired
-    public CategoryController(CategoryProvider categoryProvider, CategoryDao categoryDao) {
-        this.categoryProvider = categoryProvider;
-    }
-
     /**
-     * 전체 카테고리 조회 API
+     * 18. 카테고리 조회 API
      *
-     * @return BaseResponse<List < GetCategoryRes>>
+     * @return BaseResponse<List<GetCategoryRes>>
      */
     @GetMapping("") // (GET) /categories
     public BaseResponse<List<GetCategoryRes>> getCategories() throws BaseException {
@@ -33,18 +30,5 @@ public class CategoryController {
         getCategoriesRes = categoryProvider.getCategories();
         return new BaseResponse<>(getCategoriesRes);
     }
-
-    /**
-     * 서비스와 카테고리 조회 API
-     *
-     * @param serviceId
-     * @return BaseResponse<List < GetCategoryRes>>
-     */
-    @GetMapping("{serviceId}") // (GET) /categories/:serviceId
-    public BaseResponse<List<GetCategoryRes>> getCategoriesByServiceId(@PathVariable int serviceId) throws BaseException {
-        List<GetCategoryRes> getCategoriesRes = categoryProvider.getCategoriesByServiceId(serviceId);
-        return new BaseResponse<>(getCategoriesRes);
-    }
-
 
 }
