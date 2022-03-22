682069229f6a088139ec203f441dc4b6bff9bbf8
hyeongjun <chogudwns@naver.com>
2022-03-23 04:08:10 +0900

    feat: Implement getCategoryProducts function 카테고리에 해당하는 작품들을 호출하는 api를 구현

--- src/main/java/com/example/demo/src/product/ProductMapper.java
+++ src/main/java/com/example/demo/src/product/ProductMapper.java
@@ -2,6 +2,7 @@ package com.example.demo.src.product;
 
 import com.example.demo.src.product.model.response.GetLiveRes;
 import com.example.demo.src.product.model.response.GetNewRes;
+import com.example.demo.src.product.model.response.GetProductRes;
 import com.example.demo.src.product.model.response.GetTodayRes;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
@@ -14,4 +15,5 @@ public interface ProductMapper {
     List<GetTodayRes> getTodayProducts();
     List<GetLiveRes> getLiveProducts();
     List<GetNewRes> getNewProducts();
+    List<GetProductRes> getCategoryProducts(int categoryId);
 }
