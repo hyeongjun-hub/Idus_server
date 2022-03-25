3fb660c880feb1413622347d1fc296ef8c2cd227
hyeongjun <chogudwns@naver.com>
2022-03-26 00:00:29 +0900

    fix: cart에서 exception을 잡지못하는 버그 수정

--- src/main/java/com/example/demo/src/cart/CartService.java
+++ src/main/java/com/example/demo/src/cart/CartService.java
@@ -22,62 +22,50 @@ public class CartService {
 
     @Transactional(rollbackFor = {BaseException.class})
     public int createCart(int userId, PostCartReq postCartReq) throws BaseException {
-        try{
-            // 중복 체크
-            if(cartMapper.checkCart(userId) == 1){
-                return cartMapper.getCartId(userId);
-            }
-            int cartId = 0;
-            PostCreateCartReq postCreateCartReq = new PostCreateCartReq(userId, cartId, postCartReq.getIsDirectOrder());
-            int result = cartMapper.createCart(postCreateCartReq);
-            if (result == 0) {
-                throw new BaseException(CREATE_FAIL_CART);
-            }
-            return postCreateCartReq.getCartId();
-        } catch (Exception exception) {
-            throw new BaseException(DATABASE_ERROR);
+        // 중복 체크
+        if (cartMapper.checkCart(userId) == 1) {
+            return cartMapper.getCartId(userId);
         }
+        int cartId = 0;
+        PostCreateCartReq postCreateCartReq = new PostCreateCartReq(userId, cartId, postCartReq.getIsDirectOrder());
+        int result = cartMapper.createCart(postCreateCartReq);
+        if (result == 0) {
+            throw new BaseException(CREATE_FAIL_CART);
+        }
+        return postCreateCartReq.getCartId();
     }
 
     @Transactional(rollbackFor = {BaseException.class})
     public int createSmallCart(PostSmallCartReq postSmallCartReq) throws BaseException {
-        try{
-            int result = cartMapper.createSmallCart(postSmallCartReq);
-            if (result == 0) {
-                throw new BaseException(CREATE_FAIL_CART);
-            }
-            result = cartMapper.updateCartPrice(postSmallCartReq.getCartId(), postSmallCartReq.getPrice());
-            if (result == 0) {
-                throw new BaseException(UPDATE_FAIL_PRICE);
-            }
-            result = cartMapper.updateDeliveryTip(postSmallCartReq.getCartId(), postSmallCartReq.getDeliveryTip());
-            if (result == 0) {
-                throw new BaseException(CREATE_FAIL_DELIVERY_TIP);
-            }
-            return postSmallCartReq.getSmallCartId();
-        } catch (Exception exception) {
-            throw new BaseException(DATABASE_ERROR);
+        int result = cartMapper.createSmallCart(postSmallCartReq);
+        if (result == 0) {
+            throw new BaseException(CREATE_FAIL_CART);
         }
+        result = cartMapper.updateCartPrice(postSmallCartReq.getCartId(), postSmallCartReq.getPrice());
+        if (result == 0) {
+            throw new BaseException(UPDATE_FAIL_PRICE);
+        }
+        result = cartMapper.updateDeliveryTip(postSmallCartReq.getCartId(), postSmallCartReq.getDeliveryTip());
+        if (result == 0) {
+            throw new BaseException(CREATE_FAIL_DELIVERY_TIP);
+        }
+        return postSmallCartReq.getSmallCartId();
     }
 
     @Transactional(rollbackFor = {BaseException.class})
     public void createOrderDetail(PostOrderDetailReq postOrderDetailReq) throws BaseException {
-        try{
-            int smallCartId = postOrderDetailReq.getSmallCartId();
-            for (int i = 0; i < postOrderDetailReq.getProductOptionId().size(); i++) {
-                int result = cartMapper.createOrderDetail(postOrderDetailReq.getProductOptionId().get(i), smallCartId);
-                if (result == 0) {
-                    throw new BaseException(CREATE_FAIL_CART);
-                }
+        int smallCartId = postOrderDetailReq.getSmallCartId();
+        for (int i = 0; i < postOrderDetailReq.getProductOptionId().size(); i++) {
+            int result = cartMapper.createOrderDetail(postOrderDetailReq.getProductOptionId().get(i), smallCartId);
+            if (result == 0) {
+                throw new BaseException(CREATE_FAIL_CART);
             }
-        } catch (Exception exception) {
-            throw new BaseException(DATABASE_ERROR);
         }
     }
 
 
     public void delCart(int userCartId) throws BaseException {
-        try{
+        try {
             cartMapper.delCart(userCartId);
         } catch (Exception exception) {
             throw new BaseException(DATABASE_ERROR);
