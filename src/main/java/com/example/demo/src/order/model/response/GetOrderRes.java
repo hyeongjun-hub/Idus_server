1ea8ff2c4c43de9ebe3480fc8cf48a0b7d08c05b
hyeongjun <chogudwns@naver.com>
2022-03-26 03:05:13 +0900

    feat: 유저 장바구니 조회 api 구현

--- src/main/java/com/example/demo/src/order/model/response/GetOrderRes.java
+++ src/main/java/com/example/demo/src/order/model/response/GetOrderRes.java
@@ -2,13 +2,16 @@ package com.example.demo.src.order.model.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class GetOrderRes {
     private int orderListId;
+    private int smallCartId;
     private String createdAt;
     private int finalPrice;
     private String thumbnailImageUrl;
