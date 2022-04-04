79e5089a4a507fdb360944ef00e6f8c34875d54e
hyeongjun <chogudwns@naver.com>
2022-04-04 23:20:28 +0900

    add: first Commit

--- src/main/java/com/example/demo/config/Constant.java
+++ src/main/java/com/example/demo/config/Constant.java
@@ -1,7 +1,6 @@
 package com.example.demo.config;
 
 public class Constant {
-    public static final String REDIS_PREFIX = "test";
     public static final String SENDER_NUMBER = "01045857243";
 }
 
